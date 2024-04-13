96b1085515551b34af32ea25a52786aa2c58eb64
amartain <sz1nagszi@vasvari.org>
2024-04-13 19:58:24 +0200

    now the fetch script also works LOL (forgot to add the damn good url xd )

--- .github/fetchsaveAPIdata_DS.py
+++ .github/fetchsaveAPIdata_DS.py
@@ -5,13 +5,30 @@ from datetime import datetime
 import os
 import pytz
 
-# Fetch data
-url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+# Fetch
+url = f'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 response = requests.get(url)
-data = response.json()
+top_story_ids = json.loads(response.text)
 
-# Convert to pandas
-df = pd.DataFrame(data)
+all_story_details = []
+
+# Fetch details
+
+# VERY IMPORTANT DO NO REMOVE EVEN  IF YOUR SOUL IS IN DANGER!!!!!!111111 (formatted url W/O it the correct data wouldn't be fetched)
+story_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'
+i = 0
+
+for story_id in top_story_ids:
+    story_response = requests.get(story_url.format(story_id))
+    story_details = json.loads(story_response.text)
+    all_story_details.append(story_details)
+    i += 1
+    print(i)
+
+df = pd.DataFrame(all_story_details)
+
+df.to_csv('hacker_news_data.csv', index=False)
+
 
 # Get curr. US Central
 central = pytz.timezone('US/Central')
