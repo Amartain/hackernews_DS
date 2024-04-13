690a3e93c505f77218f29534e1e1f8b73464d9c2
amartain <sz1nagszi@vasvari.org>
2024-04-13 09:37:49 +0200

    api actions automated

---
+++ .github/fetchsaveAPIdata_DS.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import pandas as pd
+from datetime import datetime
+import os
+import pytz
+
+# Fetch data
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+response = requests.get(url)
+data = response.json()
+
+# Convert to pandas
+df = pd.DataFrame(data)
+
+# Get curr. US Central
+central = pytz.timezone('US/Central')
+now = datetime.now(central)
+file_name = now.strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
+
+
+file_path = './APIdata'
+
+# Check if the directory exists! --- else it will never work LOL
+if not os.path.exists(file_path):
+    os.makedirs(file_path)
+
+# need full file path, here combine
+full_file_path = os.path.join(file_path, file_name)
+
+# Save it
+df.to_csv(full_file_path, index=False)
